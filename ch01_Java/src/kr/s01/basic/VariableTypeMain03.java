6d4bfb685e145d5182d8ac1dac12c59971f04887
hyerimkim824 <hyrimkim824@gamil.com>
2024-08-06 15:30:22 +0900

    push test

---
+++ ch01_Java/src/kr/s01/basic/VariableTypeMain03.java
@@ -0,0 +1,9 @@
+package kr.s01.basic;
+
+public class VariableTypeMain03 {
+	public static void main(String[] args) {
+		System.out.println("====묵시적 형변환(자동 형변환)======");
+		//더 큰 자료형으로 승격이 일어나는 형태, 정보의 손실이 전혀 업스며
+	}
+
+}
