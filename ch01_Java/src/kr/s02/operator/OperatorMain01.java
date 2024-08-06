131323cac5d8b84d07133cb073b7c9bb703aa7f4
hyerimkim824 <hyrimkim824@gamil.com>
2024-08-06 17:12:36 +0900

    push 1

---
+++ ch01_Java/src/kr/s02/operator/OperatorMain01.java
@@ -0,0 +1,17 @@
+package kr.s02.operator;
+
+public class OperatorMain01 {
+	public static void main(String[] args) {
+		
+		System.out.println("====증감 연산자====");
+		
+		//증가 연산자
+		int i = 5;
+		
+		//증가 연산자를 변수명 앞에 명시하면 변수값을 1 증가시킨 후 증가된 값을 읽어옴
+		System.out.println(++i);
+				
+		
+	}
+
+}
