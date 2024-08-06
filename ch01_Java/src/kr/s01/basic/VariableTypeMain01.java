e49bf857f891862660bdd08bcedb41361cce983e
hyerimkim824 <hyrimkim824@gamil.com>
2024-08-06 14:38:38 +0900

    푸쉬2

--- ch01_Java/src/kr/s01/basic/VariableTypeMain01.java
+++ ch01_Java/src/kr/s01/basic/VariableTypeMain01.java
@@ -27,7 +27,36 @@ public class VariableTypeMain01 {
 		char c5= '\uc790'; //자를 유니코드로 표현
 		System.out.println("c5 = "+c5);
 		
+		System.out.println("====정수형====");
+		//byte, 크기 : 1byte, 표현범위 : -128~127
+		byte b1 = 127; 
+		System.out.println("b1 = " +b1);
 		
+		//short, 크기 : 2byte, 표현범위 : -32,768~32,767
+		short s1 = 32767;
+		System.out.println("s1 = "+s1);
+		
+		//int, 크기 : 4byte, 표현범위 : -2,147,483,648~2,147,483,647
+		int in1 = 1234;
+		System.out.println("in1 = "+in1);
+		
+		//JAVA system은 23같은 숫자는 자동적으로 int형 데이터가 된다.
+		//만약 23을 long 데이터로 보고 싶으면 23L로 사용해야한다.
+		
+		//long, 크기 : 8byte
+		long lg1 = 1234L;
+		System.out.println("ig1 = "+lg1);
+		
+		//float, 크기 : 4byte
+		//기본적인 실수 데이터는 double 데이터로 인식이 된다.
+		float f1 = 9.1f; // 9.1로 사용하면 에러가 난다.
+		System.out.println("f1 = "+f1);
+		
+		//double, 크기 : 8byte(소수점 이하 자리의 정밀도가 향상된다.)
+		//더 정밀도를 높이려면 다른 자료형을 사용해야 한다.
+		
+		double d1 = 9.8;
+		System.out.println("d1 = "+d1);
 		
 	}
 
