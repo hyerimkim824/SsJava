0460baeeaabfd39c831bfb26409b2e57949dfd4f
hyerimkim824 <hyrimkim824@gamil.com>
2024-08-07 12:03:58 +0900

    push 1

---
+++ ch01_Java/src/kr/s02/operator/OperatorMain03.java
@@ -0,0 +1,34 @@
+package kr.s02.operator;
+
+public class OperatorMain03 {
+	public static void main(String[] args) {
+		System.out.println("====비교(관계)연산자====");
+		boolean result;
+		int a = 10;
+		double b = 10.5;
+		
+		result = a < b;
+		System.out.println("a < b : " + result);
+		
+		result = a > b;
+		System.out.println("a > b : " + result);
+		
+		result = a >= b;
+		System.out.println("a >= b : " + result);
+		
+		result = a <= b;
+		System.out.println("a <= b : " + result);
+		
+		result = a == b;
+		System.out.println("a == b : " + result);
+		
+		result = a != b;
+		System.out.println("a != b : " + result);
+		
+	}
+}
+
+
+
+
+
