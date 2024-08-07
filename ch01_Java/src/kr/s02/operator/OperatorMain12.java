75fa95ede42d523eba6de8844b1ab684288e8012
hyerimkim824 <hyrimkim824@gamil.com>
2024-08-07 15:00:20 +0900

    실습 push

---
+++ ch01_Java/src/kr/s02/operator/OperatorMain12.java
@@ -0,0 +1,38 @@
+package kr.s02.operator;
+
+public class OperatorMain12 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * 
+		 * [실수]
+		 * 키보드에서 입력한 정수값에 마지막 자리수를 제외한 값과 마지막 자릿수를 표시하시오.
+		 * 
+		 * [입력 예시]
+		 * 정수값 : 123
+		 * 
+		 * [출력 예시]
+		 * 마지막 자릿수를 제외한 값 : 12
+		 * 마지막 자릿수 : 3
+		 */
+		
+		java.util.Scanner input = 
+				new java.util.Scanner(System.in);
+		System.out.print("정수값:");
+		int num = input.nextInt();
+		
+		int num2 = num/10;//마지막 자릿수를 제외한 값
+		int num3 = num%10;//마지막 자릿수
+		
+		System.out.println("마지막 자릿수를 제외한 값 = " + num2);	
+		System.out.println("마지막 자릿수 = " + num3);	
+		
+		
+		
+		input.close();
+	
+
+	}
+
+}
