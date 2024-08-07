41d5b005304fbcad09b32a6420c51679a9153292
hyerimkim824 <hyrimkim824@gamil.com>
2024-08-07 17:21:01 +0900

    8/7 추가

--- ch01_Java/src/kr/s02/operator/OperatorMain03.java
+++ ch01_Java/src/kr/s02/operator/OperatorMain03.java
@@ -7,7 +7,7 @@ public class OperatorMain03 {
 		int a = 10;
 		double b = 10.5;
 		
-		result = a < b;
+		result = a < b; //a와 b를 비교할 때 a가 자동적으로 double 데이터가 된다.
 		System.out.println("a < b : " + result);
 		
 		result = a > b;
