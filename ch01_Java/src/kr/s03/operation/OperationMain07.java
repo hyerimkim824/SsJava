78513664e6f4a368ab09eda5165566f44400b7e7
hyerimkim824 <hyrimkim824@gamil.com>
2024-08-08 10:54:31 +0900

    test

--- ch01_Java/src/kr/s03/operation/OperationMain07.java
+++ ch01_Java/src/kr/s03/operation/OperationMain07.java
@@ -12,6 +12,22 @@ public class OperationMain07 {
 			//a의 자료형이 int에서 float으로 자동 형변환
 			System.out.println("10과 10.0f는 같다");
 			}
+		
+		char c ='0'; //'0'의 아스키 코드 48
+		int d = 0;
+		if(c!=d) {//c : char->int로 형변환되어 48로 인식
+
+			System.out.println("'0'과 0은 같지 않다.");
+			
+		char e ='A';//'A'의 아스키 코드 65
+		int f = 65;
+		
+		if(e==f) {//e : char->int로 형변환 되어 65로 인식
+			System.out.println("'A'는 65와 같다.");
+		}
+		
+			
+		}
 	}
 
 }
