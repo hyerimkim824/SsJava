c3b550de78f7843a1400ae6a5915b6ddbd6dae50
hyerimkim824 <hyrimkim824@gamil.com>
2024-08-08 10:37:06 +0900

    test

--- ch01_Java/src/kr/s03/operation/OperationMain07.java
+++ ch01_Java/src/kr/s03/operation/OperationMain07.java
@@ -3,8 +3,15 @@ package kr.s03.operation;
 public class OperationMain07 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int a = 10;
+		float b =10.0f;
+		//만약 a,b를 연산을 하거나 비교를 한다며면 같은 데이터형으로
+		
+		if(a==b) {
+			//a의 자료형이 int에서 float으로 자동 형변환
+			System.out.println("10과 10.0f는 같다");
+			}
 	}
 
 }
