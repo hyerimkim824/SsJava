a1c5ac1377e4a549258c641f9d4d036a98535e37
hyerimkim824 <hyrimkim824@gamil.com>
2024-08-08 12:23:53 +0900

    최종ver

--- ch01_Java/src/kr/s03/operation/OperationMain10.java
+++ ch01_Java/src/kr/s03/operation/OperationMain10.java
@@ -7,7 +7,7 @@ public class OperationMain10 {
 		java.util.Scanner input=
 				new java.util.Scanner(System.in);
 		
-		int result=0;
+		int result=0; //초기화
 		
 		System.out.print("첫번째수=");
 		int first = input.nextInt();
@@ -33,20 +33,33 @@ public class OperationMain10 {
 		}
 		else if(operator.equals("/")) {
 			//입력한 연산자가 / 이면 true
-			
-			result = first/second;
+			if(second!=0) {
+			result = first/second;}
+			else {
+				System.out.println("0으로 나눌 수 없습니다.");
+				System.exit(0); //프로그램 종료
+			}
 		}
 		else if(operator.equals("%")) {
 			//입력한 연산자가 % 이면 true
+			
+			if(second!=0) {
+				result = first%second;}
+				else {
+					System.out.println("0으로 나눌 수 없습니다.");
+					System.exit(0); //프로그램 종료
+				}
 			
 			result = first%second;
 		}
 		else {
 			System.out.println("잘못된 연산자 입력");
+			
+			//프로그램 종료
+			System.exit(0);
 		}
-		System.out.println();
+		System.out.println();//단순 줄바꿈
 		System.out.printf("%d %s %d = %d",first,operator, second, result);
-		
 		
 		input.close();
 	}
