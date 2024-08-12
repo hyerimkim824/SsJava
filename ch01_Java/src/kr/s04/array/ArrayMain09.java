f6d23fd054e43953d6af37176359eb30ec726762
hyerimkim824 <hyrimkim824@gamil.com>
2024-08-12 12:51:08 +0900

    main class args

---
+++ ch01_Java/src/kr/s04/array/ArrayMain09.java
@@ -0,0 +1,13 @@
+package kr.s04.array;
+
+public class ArrayMain09 {
+
+	public static void main(String[] args) {
+		
+		//전달된 데이터는 모두 문자열로 인식하기 때문에
+		//+연산을 하지 못하고 문자열 연결로 수행
+		System.out.println(args[0]+args[1]);
+		System.out.println("-----------------")
+	}
+
+}
